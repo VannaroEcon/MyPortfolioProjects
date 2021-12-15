3bf0c26942bc681740183190506e3c99b9287b0d
VannaroEcon <69631241+VannaroEcon@users.noreply.github.com>
2021-12-15 21:09:38 +0900

    Add queries to split address, deal with dummy variable and duplicates.

--- nashville_housing_data_cleaning.sql
+++ nashville_housing_data_cleaning.sql
@@ -57,3 +57,125 @@ Where a.PropertyAddress is null
 
 
 
+--------------------------------------------------------------------------------------
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+-- Property address
+
+Select PropertyAddress
+From dbo.nashvillehousing
+
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+From dbo.nashvillehousing
+
+
+ALTER TABLE nashvillehousing
+Add PropertySplitAddress nvarchar(255);
+
+Update nashvillehousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+ALTER TABLE nashvillehousing
+Add PropertySplitCity nvarchar(255);
+
+Update nashvillehousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+
+
+-- Owner Address
+
+Select OwnerAddress
+From dbo.nashvillehousing
+
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From dbo.nashvillehousing
+
+
+ALTER TABLE nashvillehousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update nashvillehousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+
+ALTER TABLE nashvillehousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update nashvillehousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+
+ALTER TABLE nashvillehousing
+Add OwnerSplitState Nvarchar(255);
+
+Update nashvillehousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+
+
+
+--------------------------------------------------------------------------------------
+
+-- Change Y and N to Yes and No in "Sold as Vacant" column
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From dbo.nashvillehousing
+Group by SoldAsVacant
+order by 2
+
+
+
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From dbo.nashvillehousing
+
+
+Update nashvillehousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+
+
+--------------------------------------------------------------------------------------
+
+-- Remove Duplicates
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From dbo.nashvillehousing
+)
+Select *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+
+
+Select *
+From nashvillehousing
+
+

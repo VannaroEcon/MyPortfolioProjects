dacbaf21334dae2790f5d362e26073dbb3cad460
VannaroEcon <69631241+VannaroEcon@users.noreply.github.com>
2021-12-15 20:57:16 +0900

    Add query to populate property address

--- nashville_housing_data_cleaning.sql
+++ nashville_housing_data_cleaning.sql
@@ -31,3 +31,29 @@ SET SaleDateConverted = CONVERT(Date, SaleDate)
 
 
 --------------------------------------------------------------------------------------
+
+-- Populate Property Address
+
+Select *
+From dbo.nashvillehousing
+order by ParcelID
+
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From dbo.nashvillehousing a
+JOIN dbo.nashvillehousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From dbo.nashvillehousing a
+JOIN dbo.nashvillehousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+

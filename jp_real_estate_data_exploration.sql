588beba81518dab60dd6871c1cf54e49161021fd
VannaroEcon <69631241+VannaroEcon@users.noreply.github.com>
2021-12-21 22:26:16 +0900

    Find the annual growth rate of real estate price using CTE, Temp table and Views

--- jp_real_estate_data_exploration.sql
+++ jp_real_estate_data_exploration.sql
@@ -69,6 +69,8 @@ order by 2, 1, 3
 
 
 
+------------------------------------------------------------------------------------
+
 -- Growth rate of average real estate price by type and prefecture over 5-year period
 
 Select Type, Prefecture, Year, cast(AVG([Transaction-price(total)]) as float) as AveragePrice, 100 * (cast(AVG([Transaction-price(total)]) as float) - lag(cast(AVG([Transaction-price(total)]) as float), 1) over (Partition by Prefecture, Type Order by Year)) / lag(cast(AVG([Transaction-price(total)]) as float), 1) over (Partition by Prefecture, Type Order by Year) as GrowthRate
@@ -78,7 +80,7 @@ Group by Year, Type, Prefecture
 order by 2, 1, 3
 
 
--- Using temp table
+-- Using CTE
 
 With GrowthRate (Type, Prefecture, Year, AveragePrice)
 as
@@ -93,6 +95,41 @@ Select *, 100 * (AveragePrice - lag(AveragePrice, 1) over (Partition by Prefectu
 From GrowthRate
 
 
+-- Using Temp table
+
+DROP Table if exists #GrowthRate
+Create Table #GrowthRate
+(
+Type nvarchar(255),
+Prefecture nvarchar(255),
+Year int,
+AveragePrice int
+)
+
+
+Insert into #GrowthRate
+Select ts.Type, ts.Prefecture, ts.Year, cast(AVG(ts.[Transaction-price(total)]) as float) as AveragePrice
+From dbo.tokyo_saitama_prefectures as ts
+Where Year = '2010' OR Year = '2015' OR Year = '2020'
+Group by ts.Year, ts.Type, ts.Prefecture
+
+Select *, 100 * (AveragePrice - lag(AveragePrice, 1) over (Partition by Prefecture, Type Order by Year)) / lag(AveragePrice, 1) over (Partition by Prefecture, Type Order by Year) as GrowthRate
+From #GrowthRate
+GO
+
+
+-- Creating View to store data for later visualizations
+
+Create View AnnualPriceGrowthRate as
+Select Type, Prefecture, Year, cast(AVG([Transaction-price(total)]) as float) as AveragePrice, 100 * (cast(AVG([Transaction-price(total)]) as float) - lag(cast(AVG([Transaction-price(total)]) as float), 1) over (Partition by Prefecture, Type Order by Year)) / lag(cast(AVG([Transaction-price(total)]) as float), 1) over (Partition by Prefecture, Type Order by Year) as GrowthRate
+From dbo.tokyo_saitama_prefectures
+Where Year = '2010' OR Year = '2015' OR Year = '2020'
+Group by Year, Type, Prefecture
+-- order by 2, 1, 3
+
+GO
+
+
 
 ------------------------------------------------------------------------------------
 
@@ -102,6 +139,3 @@ Select Type, Prefecture, COUNT([No]) As Total
 From dbo.tokyo_saitama_prefectures
 Group by Type, Prefecture
 order by 2, 1
-
-
--- 

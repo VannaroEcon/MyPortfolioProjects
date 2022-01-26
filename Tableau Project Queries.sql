715d982e0039ed6633e4244d086335f61386f8a3
VannaroEcon <69631241+VannaroEcon@users.noreply.github.com>
2022-01-26 14:26:07 +0900

    Query additional tables

--- Tableau Project Queries.sql
+++ Tableau Project Queries.sql
@@ -9,6 +9,10 @@ USE PortfolioProject
 GO
 
 
+Select * From dbo.tokyo_saitama_prefectures
+GO
+
+
 -- Table 1
 -- Real estate price by types in Tokyo and Saitama in 2020
 
@@ -48,3 +52,28 @@ From dbo.tokyo_saitama_prefectures
 Group by Type, Prefecture
 order by 2, 1
 
+
+
+-- Table 5
+
+Select Type, Prefecture, City, [NearestStation(min)], [Transaction-price(total)], [Area(m^2)], Quarter, Year
+From dbo.tokyo_saitama_prefectures
+where Prefecture = 'Tokyo'
+order by 3, 1
+
+
+
+-- Table 6
+
+Select Type, Prefecture, City, [NearestStation(min)], [Transaction-price(total)], [Area(m^2)], Quarter, Year
+From dbo.tokyo_saitama_prefectures
+where Prefecture = 'Saitama'
+order by 3, 1
+
+
+
+-- Table 
+
+Select Type, Prefecture, City, [NearestStation(min)], [Transaction-price(total)], [Area(m^2)], Quarter, Year
+From dbo.tokyo_saitama_prefectures
+order by 3, 1
